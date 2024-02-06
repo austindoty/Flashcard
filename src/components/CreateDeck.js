4ab4c0a6c51b917fc29c388c859bce0a3b50ebf9
Austin Doty <asrdoty@gmail.com>
2024-02-06 09:41:05 -0500

    prettier

--- src/components/CreateDeck.js
+++ src/components/CreateDeck.js
@@ -3,80 +3,77 @@ import { Link, useHistory } from "react-router-dom";
 import { createDeck } from "../utils/api/index";
 
 function CreateDeck() {
-    const history = useHistory();
-    const initialState = {
-        name: "",
-        description: "",
-    };
-    
-    const [newDeck, setNewDeck] = useState(initialState);
+  const history = useHistory();
+  const initialState = {
+    name: "",
+    description: "",
+  };
 
-    function handleChange({ target }) {
-        setNewDeck({
-            ...newDeck,
-            [target.name]: target.value,
-        });
-    }
+  const [newDeck, setNewDeck] = useState(initialState);
 
-    async function handleSubmit(event) {
-        event.preventDefault();
-        const abortController = new AbortController();
-        const response = await createDeck(
-            { ...newDeck },
-            abortController.signal
-        );
-        history.push("/");
-        return response;
-    }
+  function handleChange({ target }) {
+    setNewDeck({
+      ...newDeck,
+      [target.name]: target.value,
+    });
+  }
 
-    async function handleCancel() {
-        history.push("/");
-    }
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const abortController = new AbortController();
+    const response = await createDeck({ ...newDeck }, abortController.signal);
+    history.push("/");
+    return response;
+  }
 
-    return (
-        <div>
-            <ol className="breadcrumb">
-                <li className="breadcrumb-item">
-                    <Link to="/">Home</Link>
-                </li>
-                <li className="breadcrumb-item active">Create Deck</li>
-            </ol>
-            <form onSubmit={(event) => handleSubmit(event)}>
-                <h1>Create Deck</h1>
-                <div className="form-group">
-                    <label>Name</label>
-                    <input
-                        id="name"
-                        name="name"
-                        className="form-control"
-                        onChange={handleChange}
-                        type="text"
-                        value={newDeck.name}
-                    />
-                </div>
-                <div className="form-group">
-                    <label>Description</label>
-                    <textarea
-                        id="description"
-                        name="description"
-                        className="form-control"
-                        onChange={handleChange}
-                        type="text"
-                        value={newDeck.description}
-                    />
-                </div>
-                <button
-                    className="btn btn-secondary mx-1"
-                    onClick={() => handleCancel()}
-                >
-                    Cancel
-                </button>
-                <button className="btn btn-primary mx-1" type="submit">
-                    Submit
-                </button>
-            </form>
+  async function handleCancel() {
+    history.push("/");
+  }
+
+  return (
+    <div>
+      <ol className="breadcrumb">
+        <li className="breadcrumb-item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="breadcrumb-item active">Create Deck</li>
+      </ol>
+      <form onSubmit={(event) => handleSubmit(event)}>
+        <h1>Create Deck</h1>
+        <div className="form-group">
+          <label>Name</label>
+          <input
+            id="name"
+            name="name"
+            className="form-control"
+            onChange={handleChange}
+            type="text"
+            value={newDeck.name}
+          />
         </div>
-    );
+        <div className="form-group">
+          <label>Description</label>
+          <textarea
+            id="description"
+            name="description"
+            className="form-control"
+            onChange={handleChange}
+            type="text"
+            value={newDeck.description}
+          />
+        </div>
+        <button
+          className="btn btn-secondary mx-1"
+          onClick={() => handleCancel()}
+        >
+          Cancel
+        </button>
+        <button className="btn btn-primary mx-1" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default CreateDeck;
--- src/components/EditCard.js
+++ src/components/EditCard.js
@@ -1,105 +1,105 @@
-import React, { useEffect, useState } from "react"
-import { Link, useHistory, useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Link, useHistory, useParams } from "react-router-dom";
 import { readCard, readDeck, updateCard } from "../utils/api/index";
-import CardForm from "./CardForm.js"
+import CardForm from "./CardForm.js";
+
 function EditCard() {
-    const history = useHistory();
-    const { deckId, cardId } = useParams();
-    const [currentDeck, setCurrentDeck] = useState({});
+  const history = useHistory();
+  const { deckId, cardId } = useParams();
+  const [currentDeck, setCurrentDeck] = useState({});
 
-    const initialFormState = {
-        id: -1,
-        front: "",
-        back: "",
-    };
-    const [formData, setFormData] = useState({ ...initialFormState });
+  const initialFormState = {
+    id: -1,
+    front: "",
+    back: "",
+  };
+  const [formData, setFormData] = useState({ ...initialFormState });
 
-useEffect(() => {
+  useEffect(() => {
     const abortController = new AbortController();
     async function loadDeck() {
-        try {
-            const deck = await readDeck(deckId, abortController.signal);
-            setCurrentDeck(deck);
-            console.log("I loaded the deck", deck);
-        } catch (error) {
-            if (error.name === "AbortError")
-            console.log("Aborted Load Single Deck");
-            else throw error;
-        }
-    } loadDeck();
+      try {
+        const deck = await readDeck(deckId, abortController.signal);
+        setCurrentDeck(deck);
+        console.log("I loaded the deck", deck);
+      } catch (error) {
+        if (error.name === "AbortError")
+          console.log("Aborted Load Single Deck");
+        else throw error;
+      }
+    }
+    loadDeck();
     return () => {
-    console.log("Cleanup Load Single Deck");
-    abortController.abort();
+      console.log("Cleanup Load Single Deck");
+      abortController.abort();
     };
-}, []);
+  }, []);
 
-useEffect(() => {
+  useEffect(() => {
     const abortController = new AbortController();
     async function loadCard() {
-        try {
-            const card = await readCard(cardId, abortController.signal);
-            setFormData({ ...card });
-
-        } catch (error){
-            if (error.name === "AbortError")
-            console.log("Aborted Load Single Card");
-            else throw error;
-        }
+      try {
+        const card = await readCard(cardId, abortController.signal);
+        setFormData({ ...card });
+      } catch (error) {
+        if (error.name === "AbortError")
+          console.log("Aborted Load Single Card");
+        else throw error;
+      }
     }
 
     loadCard();
     return () => {
-        abortController.abort();
+      abortController.abort();
     };
-}, []);
+  }, []);
 
-const handleChange = ({ target }) => {
+  const handleChange = ({ target }) => {
     setFormData({
-        ...formData,
-        [target.name]: target.value,
+      ...formData,
+      [target.name]: target.value,
     });
-};
+  };
 
-const handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     async function create() {
-        setFormData({ ...initialFormState }); // Reset form
-        await updateCard(formData);
-        history.push(`/decks/${deckId}`);
+      setFormData({ ...initialFormState }); // Reset form
+      await updateCard(formData);
+      history.push(`/decks/${deckId}`);
     }
     create();
-};
+  };
 
-const handleCancel = () => {
+  const handleCancel = () => {
     history.push(`/decks/${deckId}`);
-};
+  };
 
-return (
-<>
-    <div className="row">
+  return (
+    <>
+      <div className="row">
         <div className="col">
-            <p className="breadcrumb">
-                <Link to="/">
-                    <i className="bi bi-house-door"></i> Home
-                </Link>
-                    /
-                <Link to={`/decks/${deckId}`}>{currentDeck.name}</Link>
-                    / Edit Card {cardId}
-            </p>
+          <p className="breadcrumb">
+            <Link to="/">
+              <i className="bi bi-house-door"></i> Home
+            </Link>
+            /<Link to={`/decks/${deckId}`}>{currentDeck.name}</Link>/ Edit Card{" "}
+            {cardId}
+          </p>
         </div>
-    </div>
-    <div className="row">
+      </div>
+      <div className="row">
         <div className="col">
-            <h3>Edit Card</h3>
-                <CardForm
-                    handleSubmit={handleSubmit}
-                    handleChange={handleChange}
-                    formData={formData}
-                    handleCancel={handleCancel}
-                />
+          <h3>Edit Card</h3>
+          <CardForm
+            handleSubmit={handleSubmit}
+            handleChange={handleChange}
+            formData={formData}
+            handleCancel={handleCancel}
+          />
         </div>
-    </div>
-</>
-);
+      </div>
+    </>
+  );
 }
 export default EditCard;
